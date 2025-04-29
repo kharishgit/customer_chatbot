f431b8ecbf91ce42503aefdd5f04ae519b4de514
harish.k3 <harish.k@pixdynamics.com>
2025-04-29 09:20:52 +0530

    setup file added

---
+++ data_ingestion/data_transform.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from langchain_core.documents import Document
+
+
+class data_converter:
+    def __init__(self):
+        pass
+    def data_trasformation(self):
+        pass
+        
+        
