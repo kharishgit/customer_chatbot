c418908d4ab207e2682297b520726a6016413915
harish.k3 <harish.k@pixdynamics.com>
2025-04-29 15:40:35 +0530

    Changes

--- data_ingestion/data_transform.py
+++ data_ingestion/data_transform.py
@@ -4,8 +4,26 @@ from langchain_core.documents import Document
 
 class data_converter:
     def __init__(self):
-        pass
+        print("Initi")
+        self.product_data = pd.read_csv(r"/Users/harishk/Documents/Projects/ML/GenAI/customer_chatbot/data/flipkart_product_review.csv")
     def data_trasformation(self):
-        pass
+        required_columns = self.product_data.columns
+        required_columns = list(required_columns[1:])
+        product_list = []
+        for index, row in self.product_data.iterrows():
+            object = {
+                "product_name":row['product_title'],
+                "product_rating":row['rating'],
+                "product_summary":row['summary'],
+                "product_review":row['review']
+            }
+            product_list.append(object)
+        print(product_list)
+
+
+
+if __name__ == '__main__':
+    data_conv = data_converter()
+    data_conv.data_trasformation()
         
         
