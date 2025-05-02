8b87a66add6a4d52134f6cdc52b0be9883317e89
harish.k3 <harish.k@pixdynamics.com>
2025-05-02 15:32:49 +0530

    Retrieval.py added

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-    
+import uvicorn
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from dotenv import load_dotenv
+
+from langchain_core.runnables import RunnablePassthrough
+
+from langchain_core.output_parsers import StrOutputParser
+
+from langchain_core.prompts import ChatPromptTemplate
+
+from retriever.retrieval import Retriever
+
+from utils.model_loader import ModelLoader
+
+from prompt_library.prompt import PROMPT_TEMPLATES
+   
